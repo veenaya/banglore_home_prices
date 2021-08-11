33e9c78ee9f5078119c19abace4382da9b2045fe
veenaya <87847600+veenaya@users.noreply.github.com>
2021-08-11 19:28:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(debug=True) 
+    app.run() 
 
 
 
