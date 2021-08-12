8c26b7e0a9028d0acd3e00f6f87455799209c2fc
veenaya <veenayagam1996@gmail.com>
2021-08-12 10:25:07 +0800

    g

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request , render_template
-# import util
+import util
 
 
 import pickle
@@ -7,39 +7,39 @@ import json
 import numpy as np
 
 app = Flask(__name__)
-
-__locations = []
-__data_columns = None
-__model = None
-
-
-def get_estimated_price(location, sqft, bhk, bath):
-    try:
-        loc_index = __data_columns.index(location.lower())  # .index() = get index location in ()
-    except:
-        loc_index = -1
-
-    x = np.zeros(len(__data_columns))
-    x[0] = sqft
-    x[1] = bath
-    x[2] = bhk
-    if loc_index >= 0:
-        x[loc_index] = 1
-
-    return round(__model.predict([x])[0], 2)
+util.load_saved_artifacts()
+# __locations = []
+# __data_columns = None
+# __model = None
 
 
-def load_saved_artifacts():
+# def get_estimated_price(location, sqft, bhk, bath):
+#     try:
+#         loc_index = __data_columns.index(location.lower())  # .index() = get index location in ()
+#     except:
+#         loc_index = -1
+
+#     x = np.zeros(len(__data_columns))
+#     x[0] = sqft
+#     x[1] = bath
+#     x[2] = bhk
+#     if loc_index >= 0:
+#         x[loc_index] = 1
+
+#     return round(__model.predict([x])[0], 2)
+
+
+# def load_saved_artifacts():
     
-    print("loading saved artifacts...done")
+#     print("loading saved artifacts...done")
 
 
-def get_location_names():
-    return __locations
+# def get_location_names():
+#     return __locations
 
 
-def get_data_columns():
-     return __data_columns
+# def get_data_columns():
+#      return __data_columns
 
 
 
@@ -47,18 +47,18 @@ def get_data_columns():
 
 def predict_home_price():
 
-    print("loading saved artifacts...start")
-    global __data_columns
-    global __locations
+    # print("loading saved artifacts...start")
+    # global __data_columns
+    # global __locations
 
-    with open("./artifacts/columns.json", "r") as f:
-        __data_columns = json.load(f)['data_columns'] # convert from json to dic and store in data_columns variable
-        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+    # with open("./artifacts/columns.json", "r") as f:
+    #     __data_columns = json.load(f)['data_columns'] # convert from json to dic and store in data_columns variable
+    #     __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
 
-    global __model
-    if __model is None:
-        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f: # open binary format for reading
-            __model = pickle.load(f) # load pickle file and store in model variable
+    # global __model
+    # if __model is None:
+    #     with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f: # open binary format for reading
+    #         __model = pickle.load(f) # load pickle file and store in model variable
 
     if request.method == 'POST':
         total_sqft = float(request.form['uiSqft'])
@@ -67,14 +67,14 @@ def predict_home_price():
         bath = int(request.form['radio_bath'])
 
         list = {
-            'estimated_price': get_estimated_price(location, total_sqft, bhk, bath)
+            'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
         }
         for key in list:
                 lists = list[key]
                 lists = str(lists) + "Lakh"
         
         response = {
-            'locations': get_location_names()
+            'locations': util.get_location_names()
             }
         for key in response:
                 place = response[key]
@@ -82,7 +82,7 @@ def predict_home_price():
         return render_template('app.html', post=lists, responses=place)
     else:
             response = {
-            'locations': get_location_names()
+            'locations': util.get_location_names()
             }
             for key in response:
                 place = response[key]
@@ -92,7 +92,7 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     app.run(debug=True) 
-    load_saved_artifacts()
+    util.load_saved_artifacts()
 
 
 
