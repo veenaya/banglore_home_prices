7e7e7f95c8861de929ff50b63328d14821f9ad0a
veenaya <veenayagam1996@gmail.com>
2021-08-12 10:30:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,12 @@
 from flask import Flask, request , render_template
 import util
 
-
-import pickle
-import json
-import numpy as np
-
 app = Flask(__name__)
-util.load_saved_artifacts()
-# __locations = []
-# __data_columns = None
-# __model = None
-
-
-# def get_estimated_price(location, sqft, bhk, bath):
-#     try:
-#         loc_index = __data_columns.index(location.lower())  # .index() = get index location in ()
-#     except:
-#         loc_index = -1
-
-#     x = np.zeros(len(__data_columns))
-#     x[0] = sqft
-#     x[1] = bath
-#     x[2] = bhk
-#     if loc_index >= 0:
-#         x[loc_index] = 1
-
-#     return round(__model.predict([x])[0], 2)
-
-
-# def load_saved_artifacts():
-    
-#     print("loading saved artifacts...done")
-
-
-# def get_location_names():
-#     return __locations
-
-
-# def get_data_columns():
-#      return __data_columns
-
-
+#util.load_saved_artifacts()
 
 @app.route('/', methods=['GET', 'POST'])
 
 def predict_home_price():
-
-    # print("loading saved artifacts...start")
-    # global __data_columns
-    # global __locations
-
-    # with open("./artifacts/columns.json", "r") as f:
-    #     __data_columns = json.load(f)['data_columns'] # convert from json to dic and store in data_columns variable
-    #     __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
-
-    # global __model
-    # if __model is None:
-    #     with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f: # open binary format for reading
-    #         __model = pickle.load(f) # load pickle file and store in model variable
 
     if request.method == 'POST':
         total_sqft = float(request.form['uiSqft'])
